df8b38b236864dc7db95e799083434558a4428e2
Vikdemen <viktor.demen@gmail.com>
2020-01-25 01:01:01 +0300

    finding a motif

---
+++ FindingAMotifInDNA.py
@@ -0,0 +1,19 @@
+import VDBioinfo
+
+strand = input("Input the sequence")
+motif = input("Input the motif")
+results = []
+start = 0
+while True:
+    result = strand.find(motif, start)
+    if result == -1:
+        break
+    else:
+        start = result + 1
+        results.append(result)
+results = [str(x + 1) for x in results]
+formatted = ' '.join(results)
+print(formatted)
+
+# We need to find motifs in a strand of DNA
+# Answer needs to use 1-based numbering
