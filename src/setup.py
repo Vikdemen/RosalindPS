7d4dd9c82e8145210dddf07fcef18ae1bb8ff52b
Vikdemen <viktor.demen@gmail.com>
2020-02-28 02:04:19 +0300

    re-generated setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='RosalindProblemSolver',
+    version='0.0.1',
+    packages=['rps', 'rps.heredity_problems', 'rps.sequence_problems', 'rps.dynamic_programming_problems'],
+    package_dir={'': 'src'},
+    url='https://github.com/Vikdemen/RosalindPS',
+    license='MIT License',
+    author='Demenev Viktor',
+    author_email='viktor.demen@gmail.com',
+    description='Rosalind problem solver'
+)
